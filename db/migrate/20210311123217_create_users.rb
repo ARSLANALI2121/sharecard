ddf366004b0bb5ab648604f792440a23b7f582ac
ARSLANALI2121 <ARSLAN.ORAXTECH@gmail.com>
2021-03-11 19:46:09 +0500

    Create User Sign up Page With Bootstrap

---
+++ db/migrate/20210311123217_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :password_confirmation
+      t.string :first_name
+      t.string :last_name
+      t.integer :phone_number
+      t.string :company
+      t.boolean :news_letter
+      t.timestamps
+    end
+  end
+end
