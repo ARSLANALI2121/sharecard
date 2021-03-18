93bee6a20fdba0874bedc3bdfb35e0c923ba56f1
ARSLANALI2121 <ARSLAN.ORAXTECH@gmail.com>
2021-03-18 13:07:03 +0500

    resolve the unknow attribute error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,10 @@ class UsersController < ApplicationController
 	def index
 	end
 	def create
-		
+
 		@user= User.new(user_params)
 		if @user.save
-			session[:user_id] = @user.id
+			#session[:user_id] = @user.id
 			flash.now[:success]= "User was successfully created."
 			# format.html { redirect_to @user, notice: "User was successfully created." }
 			# format.json { render :show, status: :created, location: @user }
