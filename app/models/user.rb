97e32874959ac70e6c29a10c80930c2db2794a0d
ARSLANALI2121 <ARSLAN.ORAXTECH@gmail.com>
2021-03-18 13:14:26 +0500

    Try to Resolve the user_name error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 	validates :email, presence: true
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	#validates_inclusion_of :news_letter, :in => [true, false]
-	validates :user_name, presence: true
+	#validates :user_name, presence: true
 	validates :company, presence: true
 	validates :user_name, uniqueness: true
 	#validates_uniqueness_of :first_name, scope: [:last_name, :phone_number]
