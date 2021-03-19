d0ca5b0b6c24dc47de9a39b83d6fcfbb5dbc86c6
ARSLANALI2121 <ARSLAN.ORAXTECH@gmail.com>
2021-03-19 13:14:11 +0500

    Client Side Validation solved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
-	validates :email, presence: true
-	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-	#validates_inclusion_of :news_letter, :in => [true, false]
-	validates :user_name, presence: true
-	validates :company, presence: true
-	validates :user_name, uniqueness: true
-	#validates_uniqueness_of :first_name, scope: [:last_name, :phone_number]
+	validates :user_name, presence: true, length: {minimum:3, maximum: 10 }
+	validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, presence: true
+	validates :last_name, :first_name, :phone_number, :company, :news_letter, presence: true
 
-	before_create { generate_token(:auth_token) }
-
-	def generate_token(column)
-		begin
-			self[column] = SecureRandom.urlsafe_base64
-		end while User.exists?(column => self[column])
-	end
 end
